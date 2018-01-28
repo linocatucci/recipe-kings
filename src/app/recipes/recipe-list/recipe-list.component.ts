a956b53cf77183e3d9d46cfd2d2bf1558dfd6cab
linocatucci <>
2018-01-28 11:12:12 +0100

    nav bar and recipe array and model

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-recipe-list',
@@ -7,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RecipeListComponent implements OnInit {
 
-  constructor() { }
+  recipes = [];
+
+  constructor() {
+  }
 
   ngOnInit() {
   }
